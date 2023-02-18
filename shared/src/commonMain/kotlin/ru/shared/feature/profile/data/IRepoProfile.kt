b66c4d151bca6ce8759bef65093de8c586d85bc8
danil <danilka.eremin@gmail.com>
2023-02-18 11:05:15 +0800

    получение профиля через флоу

--- shared/src/commonMain/kotlin/ru/shared/feature/profile/data/IRepoProfile.kt
+++ shared/src/commonMain/kotlin/ru/shared/feature/profile/data/IRepoProfile.kt
@@ -1,7 +1,10 @@
 package ru.shared.feature.profile.data
 
+import kotlinx.coroutines.flow.Flow
+import ru.shared.core.model.wrapper.FlowResponse
+import ru.shared.core.model.wrapper.ResponseRequest
 import ru.shared.feature.profile.data.model.PresentationProfile
 
 interface IRepoProfile {
-    suspend fun getProfile(): PresentationProfile
+    suspend fun getProfile(): Flow<FlowResponse<PresentationProfile>>
 }
--- shared/src/commonMain/kotlin/ru/shared/feature/profile/data/RepoProfileImpl.kt
+++ shared/src/commonMain/kotlin/ru/shared/feature/profile/data/RepoProfileImpl.kt
@@ -1,12 +1,15 @@
 package ru.shared.feature.profile.data
 
-import ru.commons.feature.profile.data.database.DaoProfile
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import ru.commons.feature.profile.data.database.IDaoProfile
 import ru.commons.feature.profile.data.database.Profile
 import ru.commons.feature.profile.data.database.entity.ProfileEntity
+import ru.shared.core.model.wrapper.FlowResponse
+import ru.shared.core.model.wrapper.ResponseRequest
+import ru.shared.core.model.wrapper.provideCatch
 import ru.shared.feature.profile.api.IApiProfile
 import ru.shared.feature.profile.api.response.ProfileResponse
-import ru.shared.feature.profile.data.ktor.ApiProfileImpl
 import ru.shared.feature.profile.data.ktor.toPresent
 import ru.shared.feature.profile.data.model.PresentationProfile
 
@@ -14,10 +17,24 @@ class RepoProfileImpl(
     private val api: IApiProfile,
     private val daoProfile: IDaoProfile
 ): IRepoProfile {
-    override suspend fun getProfile(): PresentationProfile {
-        //val response =  api.getProfile()
-        //daoProfile.setProfile(response.toCache())
-        return daoProfile.getProfile("kQGFnUBFx5").toPresent()
+    override suspend fun getProfile(): Flow<FlowResponse<PresentationProfile>> = flow {
+        emit(FlowResponse.Loading())
+
+        emit( FlowResponse.Success(daoProfile.getProfile("").toPresent()) )
+
+        when(val response = provideCatch {  api.getProfile() }){
+            is ResponseRequest.Error -> {
+                emit(FlowResponse.Error(response.error))
+            }
+            is ResponseRequest.Success -> {
+                daoProfile.setProfile(response.data.toCache())
+                emit( FlowResponse.Success(response.data.toPresent()) )
+            }
+        }
+
+
+
+        emit(FlowResponse.Loading(false))
     }
 }
 
