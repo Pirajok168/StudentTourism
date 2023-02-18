b0b1e9447f5aff6f2459f1ef74fbee7d50c0b3e2
danil <danilka.eremin@gmail.com>
2023-02-18 17:15:54 +0800

    фикс вылета связанным с датостор

--- shared/src/commonMain/kotlin/ru/shared/core/datastore/DataStoreModule.kt
+++ shared/src/commonMain/kotlin/ru/shared/core/datastore/DataStoreModule.kt
@@ -1,20 +1,43 @@
 package ru.shared.core.datastore
 
+import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import kotlinx.atomicfu.locks.SynchronizedObject
+import kotlinx.atomicfu.locks.synchronized
+import okio.Path
 import okio.Path.Companion.toPath
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.instance
 import org.kodein.di.singleton
 
+
+
+private lateinit var dataStore: DataStore<Preferences>
+
+private val lock = SynchronizedObject()
+fun getDataStore(producePath: () -> String): DataStore<Preferences> =
+    synchronized(lock) {
+        if (::dataStore.isInitialized) {
+            dataStore
+        } else {
+            PreferenceDataStoreFactory.createWithPath(produceFile = { producePath().toPath() })
+                .also { dataStore = it }
+        }
+    }
+
 internal val dataStoreModule = DI.Module("DataStoreModule") {
+
+
+
     bind<IAuthSettingsRepository>() with singleton {
         AuthSettingsRepository(
-            dataStore = PreferenceDataStoreFactory.createWithPath(produceFile = {
-                fileSystem(
+            dataStore = getDataStore{
+                return@getDataStore fileSystem(
                     platformConfiguration = instance()
-                ).path.toPath()
-            })
+                ).path
+            }
         )
     }
 }
